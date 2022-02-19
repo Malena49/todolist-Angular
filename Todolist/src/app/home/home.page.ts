12854bf7663f410e2c9f1f35ac05c3b06d772833
Mathieu Figari <malenazeng@gmail.com>
2022-02-19 21:07:49 +0100

    add task ok

--- Todolist/src/app/home/home.page.ts
+++ Todolist/src/app/home/home.page.ts
@@ -9,14 +9,36 @@ import { DataService } from '../services/data.service';
 })
 export class HomePage {
   currentDate: string;
+  updatedDate: string;
+  tasks = [];
+  myTitleTask = "";
+  myDescriptionTask = "";
+  addTaskBool: boolean;
+
 
   constructor(private dataService: DataService) {
-    this.dataService.getNotes().subscribe(res => {
-      console.log(res)
+    this.dataService.getTasks().subscribe(res => {
+    this.tasks = res; 
     })
     const date = new Date();
    this.currentDate = date.toLocaleDateString('fr-FR');
   }
 
+  showForm() {
+    this.addTaskBool = !this.addTaskBool;
+    this.myTitleTask = '';
+    this.myDescriptionTask = '';
+  }
+
+
+ addTask() {
+   if(this.myTitleTask === "") {
+     alert("veuillez entrer un titre")
+   } else {  
+ this.dataService.addTask({title: this.myTitleTask, description: this.myDescriptionTask, create_date: this.currentDate, update_date: this.currentDate})
+this.showForm();
+   }
+
+}
 
 }
--- Todolist/src/app/services/data.service.ts
+++ Todolist/src/app/services/data.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from "@angular/core";
 import { collectionData, Firestore } from "@angular/fire/firestore";
-import { collection } from "firebase/firestore";
+import { addDoc, collection } from "firebase/firestore";
+
+export interface Task {
+    id?: string;
+    title: string;
+    description: string;
+    create_date: string;
+    update_date: string;
+}
+
 
 @Injectable({
     providedIn: 'root'
@@ -9,8 +18,13 @@ export class DataService {
 
     constructor(private firestore: Firestore) { }
 
-    getNotes() {
-        const notesRef = collection(this.firestore, 'tasks')
-        return collectionData(notesRef);
+    getTasks() {
+        const taskRef = collection(this.firestore, 'tasks')
+        return collectionData(taskRef, { idField: 'id'});
+    }
+
+    addTask(task: Task) {
+        const tasksRef = collection(this.firestore, 'tasks');
+        return addDoc(tasksRef, task)
     }
 }
